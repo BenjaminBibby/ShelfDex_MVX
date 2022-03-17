6827c5d3ecabdd5097ed4222e2b0654b38db1c1f
Benjamin Bibby <www.benjaminbibby@hotmail.com>
2022-03-17 20:33:35 +0100

    MVX Scaffold init

---
+++ ShelfDexMvx/src/ShelfDexMvx.Core/ViewModels/Main/MainViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShelfDexMvx.Core.ViewModels.Main
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public string Title { get; set; } = "Title";
+    }
+}
