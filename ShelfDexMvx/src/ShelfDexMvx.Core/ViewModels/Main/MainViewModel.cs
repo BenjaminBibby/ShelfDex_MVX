8ec19f5642b15fa7d5e32253e66123dd78acbea7
Benjamin Bibby <www.benjaminbibby@hotmail.com>
2022-07-28 12:40:03 +0200

    MvxInteraction fixed

--- ShelfDexMvx/src/ShelfDexMvx.Core/ViewModels/Main/MainViewModel.cs
+++ ShelfDexMvx/src/ShelfDexMvx.Core/ViewModels/Main/MainViewModel.cs
@@ -1,11 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MvvmCross.Base;
+using MvvmCross.ViewModels;
 
 namespace ShelfDexMvx.Core.ViewModels.Main
 {
+    public class YesNoQuestion
+    {
+        public Action<bool> YesNoCallback { get; set; }
+        public string Question { get; set; }
+    }
+
     public class MainViewModel : BaseViewModel
     {
+        
+        private MvxInteraction<YesNoQuestion> _interaction = new MvxInteraction<YesNoQuestion>();
+        public IMvxInteraction<YesNoQuestion> Interaction => _interaction;
+
+
+        private void DoFinishProfilCommand()
+        {
+            var request = new YesNoQuestion
+            {
+                YesNoCallback = (ok) =>
+                {
+                    Console.WriteLine(ok ? "Yes" : "No");
+                },
+                Question = "This is a question"
+            };
+
+            _interaction.Raise(request);
+        }
+
         private string _title;
         public string Title
         {
--- ShelfDexMvx/src/ShelfDexMvx.Droid/Views/Main/MainFragment.cs
+++ ShelfDexMvx/src/ShelfDexMvx.Droid/Views/Main/MainFragment.cs
@@ -14,6 +14,8 @@ using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
 using AndroidX.ConstraintLayout.Widget;
 using MvvmCross.Binding.BindingContext;
+using MvvmCross.Base;
+using MvvmCross.ViewModels;
 
 namespace ShelfDexMvx.Droid.Views.Main
 {
@@ -22,21 +24,46 @@ namespace ShelfDexMvx.Droid.Views.Main
     {
         protected override int FragmentLayoutId => Resource.Layout.fragment_main;
 
+        IMvxInteraction<YesNoQuestion> _interaction;
+        IMvxInteraction<YesNoQuestion> Interaction
+        {
+            get => _interaction;
+            set
+            {
+                if (_interaction != null)
+                    _interaction.Requested -= OnInteractionRequested;
+
+                _interaction = value;
+                _interaction.Requested += OnInteractionRequested;
+            }
+        }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            View view = base.OnCreateView(inflater, container, savedInstanceState);//inflater.Inflate(FragmentLayoutId, container, false);
+            View view = base.OnCreateView(inflater, container, savedInstanceState);
 
             TextView tvWelcome = view.FindViewById<TextView>(Resource.Id.txt_welcome);
-            using (var set = this.CreateBindingSet<MainFragment, MainViewModel>())
-            {
-                set.Bind(tvWelcome).For(x => x.Text).To(vm => vm.Title).WithFallback("<MISSING STRING>");
-            }
-            //set?.Apply();
 
-            ViewModel.Title = "Title";
+            Button btnTest = view.FindViewById<Button>(Resource.Id.btn_test);
+
+            var set = this.CreateBindingSet<MainFragment, MainViewModel>();
+            set.Bind(tvWelcome).For(x => x.Text).To(vm => vm.Title).WithFallback("<MISSING STRING>").TwoWay();
+            set.Bind(this).For(view => view.Interaction).To(viewModel => viewModel.Interaction).OneWay();
+            //set.Bind(btnTest).For(x => x.Click).To(vm => vm.Interaction).OneWay();
+            set.Apply();
+
+            //ViewModel.Title = "Title";
 
             return view;
-            //base.OnCreateView(inflater, container, savedInstanceState);
+        }
+
+        private async void OnInteractionRequested(object sender, MvxValueEventArgs<YesNoQuestion> eventArgs)
+        {
+            var yesNoQuestion = eventArgs.Value;
+            // show dialog
+            //var status = await this.Activity. ShowDialog(yesNoQuestion.Question);
+            //var dialog = AlertDialog.Builder
+            //yesNoQuestion.YesNoCallback(status == DialogStatus.Yes);
         }
     }
 }
